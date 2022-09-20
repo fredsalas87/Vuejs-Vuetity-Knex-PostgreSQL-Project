76c2fd865559811835613962499d9da9622a969d
fredsalas87 <fredsalas87@gmail.com>
2022-09-19 23:26:22 -0300

    Desenvolvimento das rotas PUT, DELETE por Id

--- api/employee-api/src/controllers/employee.controller.js
+++ api/employee-api/src/controllers/employee.controller.js
@@ -21,8 +21,41 @@ exports.createEmployee = async(req, res) => {
   });
 };
 
-// ==> Método responsável por criar listar todos os 'Employees'
+// ==> Método responsável por listar todos os 'Employees'
 exports.listAllEmployees = async(req, res) => {
   const response = await db.query('SELECT * FROM employee ORDER BY name ASC');
   res.status(200).send(response.rows);
+}
+
+// ==> Método responsável por listar um determinado 'Employee' por ID
+exports.findEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  const response = await db.query('SELECT * FROM employee WHERE employee_id = $1', [employeeId]);
+  res.status(200).send(response.rows);
+}
+
+// ==> Método responsável por atualizar um determinado 'Employee' por ID
+exports.updateEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  const { name, job_role, birth, employee_registration, email, password } = req.body;
+
+  const response = await db.query(
+      `UPDATE employee 
+        SET name = $1, 
+            job_role = $2, 
+            birth = $3, 
+            employee_registration = $4, 
+            email = $5, 
+            password = $6 
+        WHERE employee_id = $7`,
+      [name, job_role, birth, employee_registration, email, password, employeeId]
+    );
+  res.status(200).send({ message: 'Employee Updated Successfully!'});
+}
+
+// ==> Método responsável por excluir um determinado 'Employee' por ID
+exports.deleteEmployeeById = async(req, res) => {
+  const employeeId = req.params.id;
+  await db.query('DELETE FROM employee WHERE employee_id = $1',[employeeId]);
+  res.status(200).send({ message: 'Employee Deleted Successfully!'});
 }
--- api/employee-api/src/routes/employee.routes.js
+++ api/employee-api/src/routes/employee.routes.js
@@ -8,10 +8,19 @@ const employeeController = require('../controllers/employee.controller');
 
 // ==> Definindo as rotas do CRUD - 'Employee'
 
-// ==> Rota responsável por criar um novo 'Coloaborador(a)': (POST): localhost:3000/api/employees
+// ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
 router.post('/employees', employeeController.createEmployee);
 
-// ==> Rota responsável por listar todos 'Coloaboradores': (GET): localhost:3000/api/employees
+// ==> Rota responsável por listar todos 'Colaboradores': (GET): localhost:3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
+
+// ==> Rota responsável por listar um determinado 'Colaborador' por ID: (GET): localhost:3000/api/employees/id
+router.get('/employees/:id', employeeController.findEmployeeById);
+
+// ==> Rota responsável por atualizar um determinado 'Colaborador(a)' por ID: (PUT): localhost:3000/api/employees/id
+router.put('/employees/:id', employeeController.updateEmployeeById);
+
+// ==> Rota responsável por excluir um determinado 'Colaborador(a)' por ID: (DELETE): localhost:3000/api/employees/id
+router.delete('/employees/:id', employeeController.deleteEmployeeById);
 
 module.exports = router
